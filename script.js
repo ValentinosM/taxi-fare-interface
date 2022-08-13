41452e9aa07ccc6f3d9f1e1715e1e5d20c99781a
Valentinos Mouxouris <v.mouxouris@hotmail.com>
2022-08-13 12:01:10 +0100

    pushing to master

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoidmFsZW50aW5vc20iLCJhIjoiY2w2cnI4dTZvMGx2eTNkcGJhb3kxNml5biJ9.FyWaZ1J4bNl-MsYNKRttgA';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
